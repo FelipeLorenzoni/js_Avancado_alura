9669834466160b45b01f56f8c21f0fe1dc141c5e
Felipe Lorenzoni <florenzoni@brivia.com.br>
2019-01-24 17:23:17 -0200

    aula 1 - arrow function, mantendo contexto do método utilizado

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -7,7 +7,9 @@ class NegociacaoController {
         this._inputData = $('#data');
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
-        this._listaNegociacoes = new ListaNegociacoes();
+        //arrow function passando contexto da chamada (lexico)
+        this._listaNegociacoes = new ListaNegociacoes(model => this._negociacoesView.update(model));
+        
         this._negociacoesView = new NegociacoesView($('#negociacoesView'));
 
         this._negociacoesView.update(this._listaNegociacoes);
@@ -19,7 +21,6 @@ class NegociacaoController {
 
     apaga(){
         this._listaNegociacoes.esvazia();
-        this._negociacoesView.update(this._listaNegociacoes);
         this._mensagem.texto = 'Negociaçoes apagadas com sucesso!';
         this._mensagemView.update(this._mensagem);
     }
@@ -27,7 +28,6 @@ class NegociacaoController {
         event.preventDefault();
     
         this._listaNegociacoes.adiciona(this._criaNegociacao());
-        this._negociacoesView.update(this._listaNegociacoes); 
 
         this._mensagem.texto = 'Negociação adicionada com sucesso';
         this._mensagemView.update(this._mensagem);
