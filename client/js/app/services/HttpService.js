c663fb6077affef6b4743a902c7546303eb83430
Felipe Lorenzoni <florenzoni@brivia.com.br>
2019-02-14 16:12:17 -0200

    adiciona fetch para compatibilidade com outros navegadores

--- client/js/app/services/HttpService.js
+++ client/js/app/services/HttpService.js
@@ -1,51 +1,25 @@
 class HttpService {
 
-
+    _handleErrors(res){
+        if(!res.ok) throw new Error(res.statusText)
+        return res;
+    }
     get(url) {
         
-        return new Promise((resolve, reject) => {
-            
+        return fetch(url)
+            .then(res => this._handleErrors(res))
+            .then(res => res.json());
 
-            let xhr = new XMLHttpRequest();
-            
-            xhr.open('GET', url);
-            
-            xhr.onreadystatechange = () => {
-                    
-                if(xhr.readyState == 4) {
-                    
-                    if(xhr.status == 200) {   
-                        
-                        resolve(JSON.parse(xhr.responseText));  
-                    } else {
-                        
-                        reject(xhr.responseText);
-                    }
-                }
-            };
-            
-            xhr.send();       
-            
-        });
     }
 
     post(url,dado){
 
-        return new Promise((resolve,reject) => {
-            let hxr = new XMLHttpRequest();
-            xhr.open("POST", url, true);
-            xhr.setRequestHeader("Content-Type","application/json");
-            xhr.onreadystatechange = () => {
-                if(xhr.readyState == 4 ){
-                    if(hxr.status == 200){
-                        resolve(JSON.parse(xhr.responseText));
-                    }
-                }else{
-                    reject(xhr.responseText);
-                }
-            };
-            xhr.send(JSON.stringify(dado));
-        });
+        return fetch(url, {
+            headers: { 'Content-type' : 'application/json'},
+            method: 'post',
+            body: JSON.stringify(dado)
+        })
+        .then(res => this._handleErrors(res));
 
     }
  
